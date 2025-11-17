d8cd48cd4197b8ecf0a020d3d7da63540af3943f
t1ctac19 <2497735@cmontmorency.qc.ca>
2025-11-17 12:44:51 -0500

    test +modification

---
+++ src/js/bootstrap.js
@@ -0,0 +1,3 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.documentElement.setAttribute("data-bs-theme", "dark");
+  });
---
+++ src/js/chartjs.js
@@ -0,0 +1,6 @@
+import Chart from "chart.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+
+
+});
